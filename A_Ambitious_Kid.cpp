6dd9e0ee0db9ebd95ed288f09e75b5c390d1d584
rhmunna143 <rhmunna19@gmail.com>
2025-06-05 10:59:22 +0600

    easy problem solved

---
+++ A_Ambitious_Kid.cpp
@@ -0,0 +1,70 @@
+/***
+ * Objective:
+ *
+ * Context:
+ *
+ *
+ * Constrains:
+ *
+ *
+ * Note:
+ *
+ *
+ * TODO:
+ * Algorithm | Pseudo-code | Steps -- Write here
+ *
+ * 1.
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int min = INT_MAX;
+
+    // absolute minimum value
+    for (int i = 0; i < n; i++)  // Fixed: Start from index 0, not 1
+    {
+        int absValue = abs(a[i]);  // Use abs() function instead of conditional negation
+        
+        if (absValue < min)
+        {
+            min = absValue;
+        }
+    }
+
+    cout << min << endl;
+
+    return 0;
+}
+
+/***
+ * Author:
+ * Md Rabbi Haque Munna
+ * BSc. [Engg.] in Computer Science and Engineering, [CSE]
+ * Rabindra Maitree University, RMU
+ * Batch: 2022-02
+ * Kushtia, Bangladesh
+ *
+ * Email: rhmunna19@gmail.com
+ * WhatsApp: +8801877-255595
+ * LinkedIn: https://www.linkedin.com/in/rhmunna143
+ * GitHub: https://github.com/rhmunna143
+ * Portfolio: https://munna-dev.web.app
+ * Facebook Profile: https://www.facebook.com/Munna.RMU
+ *
+ * Copyright (c) All rights reserved.
+ */
---
+++ A_Easy_Problem.cpp
@@ -0,0 +1,63 @@
+/***
+ * Objective:
+ *
+ * Context:
+ *
+ *
+ * Constrains:
+ *
+ *
+ * Note:
+ *
+ *
+ * TODO:
+ * Algorithm | Pseudo-code | Steps -- Write here
+ *
+ * 1.
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int cnt = 0;
+
+        for (int a = n - 1; a > 0; a--)
+        {
+            cnt++;
+        }
+
+        cout << cnt << endl;
+    }
+
+    return 0;
+}
+
+/***
+ * Author:
+ * Md Rabbi Haque Munna
+ * BSc. [Engg.] in Computer Science and Engineering, [CSE]
+ * Rabindra Maitree University, RMU
+ * Batch: 2022-02
+ * Kushtia, Bangladesh
+ *
+ * Email: rhmunna19@gmail.com
+ * WhatsApp: +8801877-255595
+ * LinkedIn: https://www.linkedin.com/in/rhmunna143
+ * GitHub: https://github.com/rhmunna143
+ * Portfolio: https://munna-dev.web.app
+ * Facebook Profile: https://www.facebook.com/Munna.RMU
+ *
+ * Copyright (c) All rights reserved.
+ */

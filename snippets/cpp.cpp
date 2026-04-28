8a7f9857c1c0cfd260a2c673946cc8716dc3249e
rhmunna143 <rhmunna19@gmail.com>
2026-04-28 23:57:52 +0600

    Add .gitignore and new C++ problem snippets

---
+++ snippets/cpp.cpp
@@ -0,0 +1,37 @@
+// Author: @rhmunna143
+
+/**
+ * Problem Statement:
+ *
+ * Constraints:
+ *
+ * Input:
+ *
+ * TODO: /  Algorithm:
+ *
+ * Output:
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    // start coding from here
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Author: @rhmunna143
